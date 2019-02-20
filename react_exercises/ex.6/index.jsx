477cada6b1001b6d65793415a0efd7b92fa08b07
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-02-19 21:56:38 -0400

    Creating components with classes Using state in component and extends the react component method Understand the importance and diference of "this" in diferents scopes Component controll and uncontroll

--- react_exercises/ex.6/index.jsx
+++ react_exercises/ex.6/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Field from './field';
+import ClassComponent from './classComponent';
 
 ReactDOM.render(
-    <Field initialValue='Teste' />
+    <ClassComponent value='Componente classe' />
     , document.getElementById('app'));
 
