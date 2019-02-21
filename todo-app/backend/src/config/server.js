35bbada6b2c1087f504742b6f52b9a24601931de
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-02-21 00:06:10 -0400

    Able cors midware to allow others aplications connect Adding new scripts to manipulate the server with pm2

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -4,8 +4,11 @@ const express = require('express');
 const bodyParse = require('body-parser');
 const server = express();
 
+const allowCors = require('./cors');
+
 server.use(bodyParse.urlencoded({extended: true}));
 server.use(bodyParse.json());
+server.use(allowCors);
 
 server.listen(PORT, () => {
     console.log(`Server runing in port ${PORT}`);
