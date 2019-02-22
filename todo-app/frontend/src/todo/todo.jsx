ad9c7552b85a483dd426bac1892112dc407e3947
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-02-22 17:28:22 -0400

    Using events Onchange and pass props by tag father "Todo"

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -8,13 +8,27 @@ export default class Todo extends Component{
 
     constructor(props){
         super(props);
+        this.state = { description: '', list: []}
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleAdd = this.handleAdd.bind(this); // this is realy importante when we want use this im method ("this" belong now the class )
+    }
+
+    handleAdd(){
+        console.log(this.state.description);
+    }
+
+    handleChange(event){
+        this.setState({...this.state, description: event.target.value})
     }
 
     render(){
         return(
             <div>
                <PageHeader name="Tarefas" small="Cadastro" ></PageHeader>
-               <TodoForm/>
+               <TodoForm handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}
+                    description={this.state.description}/>
                <TodoList/>
             </div>
         )
--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -5,10 +5,13 @@ import ButtonIcon from '../template/iconButton';
 export default props => (
     <div role='form' className="todoForm row">
         <Grid cols='12 9 10' >
-            <input id="description" className="form-control" placeholder="Adicione uma tarefa" type="text"/>
+            <input id="description" className="form-control"
+            onChange={props.handleChange}
+            value={props.description}
+            placeholder="Adicione uma tarefa" type="text"/>
         </Grid>
         <Grid cols='12 3 2' >
-            <ButtonIcon style="primary" icon="plus"></ButtonIcon>
+            <ButtonIcon style="primary" icon="plus" onClick={props.handleAdd} ></ButtonIcon>
         </Grid>
     </div>
 )
