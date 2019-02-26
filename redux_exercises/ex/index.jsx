184115db1d14b257241f7142be4c50f337d0cba5
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-02-25 21:02:53 -0400

    First example in react with redux

---
+++ redux_exercises/ex/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { combineReducers, createStore } from 'redux'; // combinereducer create a pointer for all states and the amount of this is create a store like a component imutable but who can update
+import { Provider } from 'react-redux'; // Could pass the states for components interns
+
+import Field from './field';
+
+const reducers  = combineReducers({
+    field: () => ({ value: 'Opa' })
+})
+
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <Field initialValue='Teste' />
+    </Provider>
+,   document.getElementById('app'));
+
---
+++ redux_exercises/webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './ex/index.jsx',
+    output:{
+        path: __dirname +'/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders : [{
+            test: /.jsx?$/, //the ide know better that you works with react,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets : ['es2015', 'react'],
+                plugins : ['transform-object-rest-spread']
+            }
+
+        }]
+    }
+}
