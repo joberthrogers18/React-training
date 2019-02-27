9508eb59b97c1ab7a1712a94d4cd9ba747c014c5
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-02-27 19:51:56 -0400

    Using the middleware from redux to make request asnyc in dabase

--- todo-app_redux/frontend/src/todo/todoForm.jsx
+++ todo-app_redux/frontend/src/todo/todoForm.jsx
@@ -1,40 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Grid from '../template/grid';
 import ButtonIcon from '../template/iconButton';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { changeDescription } from './todoActions';
+import { changeDescription, search } from './todoActions';
 
-const TodoForm = props => {
-    const keyHandler = e => {
-        if(e.key == 'Enter'){
-            e.shiftKey ? props.handleSearch() : props.handleAdd()
-        }else if(e.key == 'Escape'){
-            props.handleClear();
-        }
+class TodoForm extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.keyHandler = this.keyHandler.bind(this);
     }
 
-    return(
-        <div role='form' className="todoForm row mb-2">
-            <Grid cols='12 9 10' >
-                <input id="description" className="form-control"
-                onChange={props.changeDescription}
-                value={props.description}
-                onKeyUp={keyHandler}
-                placeholder="Adicione uma tarefa" type="text"/>
-            </Grid>
-            <Grid cols='12 3 2' >
-                <ButtonIcon style="primary" icon="plus" onClick={props.handleAdd} ></ButtonIcon>
-                <ButtonIcon style="info" icon="search" onClick={props.handleSearch} ></ButtonIcon>
-                <ButtonIcon style="outline-dark" icon="close" onClick={props.handleClear} ></ButtonIcon>
-            </Grid>
-        </div>
-    )
+    componentWillMount() {
+        this.props.search();
+    }
+
+    keyHandler(e){
+        if(e.key == 'Enter'){
+            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+        }else if(e.key == 'Escape'){
+            this.props.handleClear();
+        }
+    }
+    render(){
+        return(
+            <div role='form' className="todoForm row mb-2">
+                <Grid cols='12 9 10' >
+                    <input id="description" className="form-control"
+                    onChange={this.props.changeDescription}
+                    value={this.props.description}
+                    onKeyUp={this.keyHandler}
+                    placeholder="Adicione uma tarefa" type="text"/>
+                </Grid>
+                <Grid cols='12 3 2' >
+                    <ButtonIcon style="primary" icon="plus" onClick={this.props.handleAdd} ></ButtonIcon>
+                    <ButtonIcon style="info" icon="search" onClick={this.props.handleSearch} ></ButtonIcon>
+                    <ButtonIcon style="outline-dark" icon="close" onClick={this.props.handleClear} ></ButtonIcon>
+                </Grid>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => ({ description: state.todo.description })
 
-const mapDispatchToProps = dispatch => bindActionCreators({changeDescription}, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({changeDescription, search}, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
