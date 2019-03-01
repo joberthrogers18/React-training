5cc370fe8bf7c9f77c79c120615b5cbf84107c09
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-02-28 23:01:13 -0400

    Setup the  server and connect with mongodb local

---
+++ my-money-app/backend/src/config/server.js
@@ -0,0 +1,12 @@
+const PORT = process.env.PORT || 3003
+
+const express = require('express');
+const bodyParser = require('body-parser'); 
+const server = express();
+
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
+
+server.listen(PORT, () => {
+    console.log(`Backend server is running in port: ${PORT}`);
+});
