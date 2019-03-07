bbbeb444b75706fc15097c76383389c80330d2d8
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-03-07 16:26:47 -0400

    Creating the main files design for bilingCycle negocie rules
    Creating the base of tab header

--- my-money-app/frontend/src/billingCycles/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycles/billingCycle.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default props => (
-    <div>
-        <h1>Ciclo de pagamento</h1>
-    </div>
-)
+import ContentHead from '../common/template/contentHeader';
+import Content from '../common/template/content';
+import Tabs from '../common/tab/tab';
+import TabsHeader from '../common/tab/tabsHeader';
+import TabsContent from '../common/tab/tabsContent';
+import TabHead from '../common/tab/tabHeader';
+
+export default class BillingCycles extends Component{
+
+    render(){
+        return(
+            <div>
+                <ContentHead title='Ciclo de pagamentos' small='Cadastro'/>
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHead label='Listar' icon='bars'  target='tabList'/>
+                            <TabHead label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHead label='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHead label='Exclur' icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>  
+        )
+    }
+
+}
