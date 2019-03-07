b2f08c7545a64092a3d0fd7b88b7806101a805fa
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-03-06 23:04:45 -0400

    Adding the valuebox in dashboard and configurate the redux

--- my-money-app/frontend/src/index.jsx
+++ my-money-app/frontend/src/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
 
 import App from './main/app';
+import reducers from './main/reducers';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-    <App></App>,
-    document.getElementById('app')
+    <Provider store={store}>
+        <App></App>
+    </Provider>
+    ,document.getElementById('app')
 );
