2eed1b83c8a50f176e3bd4121d298f87ba82982a
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-03-07 18:46:39 -0400

    Adding the content tab base in billingCycle

--- my-money-app/frontend/src/billingCycles/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycles/billingCycle.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';  
 
 import ContentHead from '../common/template/contentHeader';
 import Content from '../common/template/content';
@@ -6,8 +8,16 @@ import Tabs from '../common/tab/tab';
 import TabsHeader from '../common/tab/tabsHeader';
 import TabsContent from '../common/tab/tabsContent';
 import TabHead from '../common/tab/tabHeader';
+import TabContent from '../common/tab/tabContent';
 
-export default class BillingCycles extends Component{
+
+import { selectTab } from '../common/tab/tabAction';
+
+class BillingCycles extends Component{
+
+    componentWillMount(){
+        this.props.selectTab('tabList')
+    }
 
     render(){
         return(
@@ -22,7 +32,10 @@ export default class BillingCycles extends Component{
                             <TabHead label='Exclur' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
                         <TabsContent>
-
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
@@ -31,3 +44,7 @@ export default class BillingCycles extends Component{
     }
 
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators( {selectTab} , dispatch);
+
+export default connect(null, mapDispatchToProps)(BillingCycles);
