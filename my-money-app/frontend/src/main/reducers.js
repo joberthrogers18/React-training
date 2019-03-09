7b81ec9529398bb45fa1b08d1a6dd23fde8a9f82
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-03-09 14:44:26 -0400

    Using the react-redux-toastr to showing the errors come in backend validation

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import {reducer as formReducer } from 'redux-form';
+import { reducer as toastrReducer} from 'react-redux-toastr';
 
 import dashboardReducer from '../dashboard/dashboardReducer';
 import tabReducer from '../common/tab/tabReducer';
@@ -9,7 +10,8 @@ const rootReducer = combineReducers({
     dashboard: dashboardReducer,
     tab: tabReducer,
     billingCycles:  BillingCycleReaducer,
-    form: formReducer //using redux form, this is work for all forms in application
+    form: formReducer, //using redux form, this is work for all forms in application
+   toastr: toastrReducer // this reducer work to appear message erros trigged by redux
 });
 
 export default rootReducer;
