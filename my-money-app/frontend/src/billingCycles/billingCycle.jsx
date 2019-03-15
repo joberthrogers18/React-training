d9504fa08d5bd78794388e81c2140d049b14ec84
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-03-14 21:43:02 -0400

    Adding button update in each element of  list billingCycles and redirect to form update

--- my-money-app/frontend/src/billingCycles/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycles/billingCycle.jsx
@@ -42,7 +42,9 @@ class BillingCycles extends Component{
                             <TabContent id='tabCreate'>
                                 <Form onSubmit={this.props.create} /> {/* When the form is submit, will call the method "create" in action creators billingcycle*/}
                             </TabContent>
-                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabUpdate'>
+                                <Form />
+                            </TabContent>
                             <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
--- my-money-app/frontend/src/billingCycles/billingCyclesAction.js
+++ my-money-app/frontend/src/billingCycles/billingCyclesAction.js
@@ -35,3 +35,10 @@ export function create(values){
     }
 
 }
+
+export function showUpdate(billingCycle){
+    return [
+        showTabs('tabUpdate'),
+        selectTab('tabUpdate')
+    ]
+}
