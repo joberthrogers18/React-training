8ff8a2fc0b7ed7e2a1555b443da06764017a59f4
Joberth Rogers <joberth.rogers18@gmail.com>
2019-09-21 15:22:26 -0400

    Finished template login

--- react_exercises/template-test-spa/spa/src/pages/Login/index.js
+++ react_exercises/template-test-spa/spa/src/pages/Login/index.js
@@ -6,7 +6,20 @@ class Login extends Component {
   
   render () {
     return (
-      <h1>Login</h1>
+      <div className="login">
+        <div className="box">
+          <p>Cadastrar novo</p>
+          <div className="sign-up">
+            <input type="text" name="name" placeholder="Nome"/>
+            <input type="type" name="observation" placeholder="Observação"/>
+            <input type="type" name="rg" placeholder="RG" />
+            <input type="password" name="password" placeholder="Senha"/>
+          </div>
+          <div className="footer">
+            <button className="submit" type="submit">Enviar</button>
+          </div>
+        </div>
+      </div>
     );
   }
 
