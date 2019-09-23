269920dfc3a786c380bf385f106b0061d6e62ed4
Joberth Rogers <joberth.rogers18@gmail.com>
2019-09-22 22:50:47 -0400

    Start the list view

--- react_exercises/template-test-spa/spa/src/pages/Main/index.js
+++ react_exercises/template-test-spa/spa/src/pages/Main/index.js
@@ -4,13 +4,46 @@ import './styles.css';
 
 class Main extends Component {
 
+  state = { 
+    name: '',
+    observation: '',
+    password: '',
+    rg:'',
+  }
+
   componentDidMount(){
-    console.log(this.props.location.state)
+    this.setState(this.props.location.state.data_login);
+  }
+
+  renderItens = () => {
+    
+    let itens = []
+    for (let i = 0; i < 5; i++){ 
+      itens.push(
+        <div key={i} className="wrapper">
+          <div className="number">{i}</div>
+          <div className="body-list">
+            <p className="name">Nome: <span>{this.state.name}</span></p>
+            <p className="observation">Observação: <span>{this.state.observation}</span></p>
+            <div className="fotter">
+              <p className="password">Senha: <span>{this.state.password}</span></p>
+              <p className="rg">RG: <span>{this.state.rg}</span></p>
+            </div>
+          </div>
+        </div>
+      )
+    }
+
+    return itens;
   }
 
   render(){
     return(
-      <h1>Main</h1>
+      <div className="list-ground">
+        <div className="list-inside">
+          {this.renderItens()}
+        </div>  
+      </div>
     )
   }
 
