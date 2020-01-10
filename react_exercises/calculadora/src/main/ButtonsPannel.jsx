4ea188e96e40930b6eaabefb7a33236d38018895
Joberth Rogers <joberth.rogers18@gmail.com>
2020-01-10 17:13:43 -0300

    Finished the calculator
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- react_exercises/calculadora/src/main/ButtonsPannel.jsx
+++ react_exercises/calculadora/src/main/ButtonsPannel.jsx
@@ -1,29 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 
 // import { Container } from './styles';
 
-export default class main extends Component {
-  render() {
-    return (
-      <div className="panel">
-        <button className="btn-max-1">AC</button>
-        <button className="signals">/</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button className="signals">*</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button className="signals"> - </button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button className="signals">+</button>
-        <button className="btn-max-2">0</button>
-        <button>.</button>
-        <button className="signals"> = </button>
-      </div>
-    );
-  }
+export default function Panel(props) {
+  return (
+    <div className="panel">
+      <button className="btn-max-1" onClick={() => props.clear("")}>
+        AC
+      </button>
+      <button className="signals" onClick={() => props.appendFunction("/")}>
+        /
+      </button>
+      <button onClick={() => props.appendFunction("7")}>7</button>
+      <button onClick={() => props.appendFunction("8")}>8</button>
+      <button onClick={() => props.appendFunction("9")}>9</button>
+      <button className="signals" onClick={() => props.appendFunction("*")}>
+        *
+      </button>
+      <button onClick={() => props.appendFunction("4")}>4</button>
+      <button onClick={() => props.appendFunction("5")}>5</button>
+      <button onClick={() => props.appendFunction("6")}>6</button>
+      <button className="signals" onClick={() => props.appendFunction("-")}>
+        {" "}
+        -{" "}
+      </button>
+      <button onClick={() => props.appendFunction("1")}>1</button>
+      <button onClick={() => props.appendFunction("2")}>2</button>
+      <button onClick={() => props.appendFunction("3")}>3</button>
+      <button className="signals" onClick={() => props.appendFunction("+")}>
+        +
+      </button>
+      <button className="btn-max-2" onClick={() => props.appendFunction("0")}>
+        0
+      </button>
+      <button onClick={() => props.appendFunction(".")}>.</button>
+      <button className="signals" onClick={props.calculateResult}>
+        {" "}
+        ={" "}
+      </button>
+    </div>
+  );
 }
--- react_exercises/calculadora/src/main/Calculator.jsx
+++ react_exercises/calculadora/src/main/Calculator.jsx
@@ -5,12 +5,38 @@ import Display from "./Display";
 import Panel from "./ButtonsPannel";
 
 export default function Calculator() {
-  const [content, useContent] = useState("");
+  const [content, setContent] = useState("");
+
+  function clear(content) {
+    setContent(content);
+  }
+
+  function appendFunction(newContent) {
+    if (String(content).trim() === "Sintaxe Invalida") {
+      setContent("");
+      return setContent(newContent);
+    }
+
+    setContent(content + newContent);
+  }
+
+  function calculateResult() {
+    try {
+      let result = eval(content);
+      setContent(result);
+    } catch (err) {
+      setContent("Sintaxe Invalida");
+    }
+  }
 
   return (
     <div className="calculator">
       <Display content={content} />
-      <Panel />
+      <Panel
+        clear={clear}
+        appendFunction={appendFunction}
+        calculateResult={calculateResult}
+      />
     </div>
   );
 }
