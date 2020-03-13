351c719192d7ca92ae6265a668be79bf18a7558a
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-12 22:22:47 -0300

    Added example using material ui

---
+++ material-test/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Users from './components/Users';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Usuários</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ material-test/src/components/Users/index.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import axios from 'axios';
+
+const columns = [
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'username', label: 'Username', minWidth: 170 },
+  { id: 'email', label: 'Email', minWidth: 170 },
+  { id: 'phone', label: 'Phone', minWidth: 170 }
+];
+
+function createUser(name, username, email, phone) {
+  return { name, username, email, phone };
+}
+
+// const rows = [
+//   createData('India', 'IN', 1324171354, 3287263),
+//   createData('China', 'CN', 1403500365, 9596961),
+//   createData('Italy', 'IT', 60483973, 301340),
+//   createData('United States', 'US', 327167434, 9833520),
+//   createData('Canada', 'CA', 37602103, 9984670),
+//   createData('Australia', 'AU', 25475400, 7692024),
+//   createData('Germany', 'DE', 83019200, 357578),
+//   createData('Ireland', 'IE', 4857000, 70273),
+//   createData('Mexico', 'MX', 126577691, 1972550),
+//   createData('Japan', 'JP', 126317000, 377973),
+//   createData('France', 'FR', 67022000, 640679),
+//   createData('United Kingdom', 'GB', 67545757, 242495),
+//   createData('Russia', 'RU', 146793744, 17098246),
+//   createData('Nigeria', 'NG', 200962417, 923768),
+//   createData('Brazil', 'BR', 210147125, 8515767),
+// ];
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
+
+export default function StickyHeadTable() {
+  const classes = useStyles();
+  const [dataSource, setDataSource] = useState([]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect( async() => {
+    let dataFiltered = [];
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    response.data.map(user => {
+      dataFiltered.push(createUser(user.name, user.username, user.email, user.phone));
+    });
+    console.log(dataFiltered);
+    setDataSource(dataFiltered);
+  }, [])
+
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {dataSource.map(row => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  {columns.map(column => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        { value }
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
+}
