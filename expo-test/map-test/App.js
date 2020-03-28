ff7a7feb80cdb662484082bc10f719b65ac1ddee
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-28 19:06:34 -0400

    Start a project using mapView from expo
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

---
+++ expo-test/map-test/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+import MapView from 'react-native-maps';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.mapStyle} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
