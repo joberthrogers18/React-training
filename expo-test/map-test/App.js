11d610681958662f7428d2c120490543e7a23439
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-30 18:08:11 -0400

    Use location in permision ang get informations

--- expo-test/map-test/App.js
+++ expo-test/map-test/App.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 import MapView from 'react-native-maps';
 
+// i18n.fallbacks = true;
+
 export default function App() {
+
+  state = {
+    location: null,
+    errorMessage: null,
+  };
+
+  const [location, setLocation] = useState('');
+  const [errorMessage, setError] = useState('');
+
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      setError('Permission to access location was denied');
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    return location;
+  };
+
+  useEffect(async () => {
+
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      setError('Oops, this will not work on Sketch in an Android emulator. Try it on your device!');
+    } else {
+      const response = await this._getLocationAsync();
+      console.log(response);
+    }
+
+  }, []);
+
   return (
     <View style={styles.container}>
       <MapView style={styles.mapStyle} />
@@ -17,10 +52,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20
+    padding: 50
   },
   mapStyle: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
 });
+
